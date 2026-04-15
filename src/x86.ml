6662832ed81fbf306bee34c6cea7c30c0e7fc001
ThatOneGin <176781942+ThatOneGin@users.noreply.github.com>
2026-04-15 14:22:29 -0300

    emit section directives at `x86.ml`

--- src/x86.ml
+++ src/x86.ml
@@ -37,6 +37,7 @@ let getreg (b: Il.bits) (r: Il.reg): string =
   match r with
   | Mreg m -> (List.nth (List.nth reg_table m) i)
   | _ -> Common.unreachable "x86_64 assembly generation" "Invalid IL register"
+;;
 
 (* to get instruction mnemonic suffix (if any) *)
 let getmnemonicsuffix (b: Il.bits): char =
@@ -45,26 +46,26 @@ let getmnemonicsuffix (b: Il.bits): char =
   | Bits16 -> 'w'
   | Bits32 -> 'l'
   | Bits64 -> 'q'
+;;
 
-let emit_indent (s: Il.smod): unit =
-  Il.smod_emit s "\t"
+let emit_indent (s: Il.smod): unit = Il.smod_emit s "\t";;
 
-let emit_newline (s: Il.smod): unit =
-  Il.smod_emit s "\n"
+let emit_newline (s: Il.smod): unit = Il.smod_emit s "\n";;
 
-let emit_imm (i: Il.imm): string =
-  Printf.sprintf "$%Ld" i
+let emit_imm (i: Il.imm): string = Printf.sprintf "$%Ld" i;;
 
 let emit_mem (m: Il.mem) (ty: Il.bits): string =
   match m with
   | Addr a -> Printf.sprintf "$.LK%d" a
   | Reg r -> "%" ^ getreg ty r
   | Stack s -> Printf.sprintf "%d(%%rbp)" s
+;;
 
 let emit_operand (o: Il.operand): string =
   match o with
   | Mem (m, t) -> emit_mem m t
   | Imm (i, _) -> emit_imm i
+;;
 
 let get_cond_suffix (o: Ast.operator) (swap: bool): string =
   if swap then
@@ -77,6 +78,7 @@ let get_cond_suffix (o: Ast.operator) (swap: bool): string =
     | Ast.OEQU -> "e"
     | Ast.ONEQ -> "ne"
     | _ -> ""
+;;
 
 (* get result of a binary expression *)
 let finish_binop (s: Il.smod) (b: Il.binop): unit =
@@ -93,6 +95,7 @@ let finish_binop (s: Il.smod) (b: Il.binop): unit =
           (Printf.sprintf "\n\tmovzb%c\t%%al, %s"
             suffix (emit_operand b.left)))
     | _ -> ()
+;;
 
 let emit_binop (s: Il.smod) (b: Il.binop): unit =
   let ins = match b.op with
@@ -110,6 +113,7 @@ let emit_binop (s: Il.smod) (b: Il.binop): unit =
       (emit_operand b.right)
       (emit_operand b.left));
   finish_binop s b
+;;
 
 let emit_jmp (s: Il.smod) (j: Il.jmp): unit =
   match j with
@@ -120,6 +124,7 @@ let emit_jmp (s: Il.smod) (j: Il.jmp): unit =
     Il.smod_emit s (Printf.sprintf "\tcmpq\t$0, %s\n" op);
     Il.smod_emit s (Printf.sprintf "\tje\t.LC%d" t.jit)
   | Jump i -> Il.smod_emit s (Printf.sprintf "jmp\t.LC%d" i)
+;;
 
 let emit_move (s: Il.smod) (m: Il.move): unit =
   let bits = Il.get_operand_type m.src in
@@ -128,6 +133,7 @@ let emit_move (s: Il.smod) (m: Il.move): unit =
       (getmnemonicsuffix bits)
       (emit_operand m.src)
       (emit_mem m.dest bits))
+;;
 
 let emit_ret (s: Il.smod) (r: Il.ret): unit = 
   let b: Il.bits = Il.type2bits r.ty in
@@ -144,6 +150,7 @@ let emit_ret (s: Il.smod) (r: Il.ret): unit =
           ("%" ^ getreg b (Mreg 0)))
     end;
     Il.smod_emit s (Printf.sprintf "\tjmp\t.LC%d" r.pc)
+;;
 
 let emit_label (s: Il.smod) (l: Il.label): unit =
   match l with
@@ -153,6 +160,7 @@ let emit_label (s: Il.smod) (l: Il.label): unit =
     | Unnamed_label id ->
       Il.smod_emit s (Printf.sprintf
         ".LC%d:" id)
+;;
 
 let emit_args (s: Il.smod) (a: Il.operand array): unit =
   let len = Array.length a in
@@ -198,12 +206,16 @@ let emit_inst (s: Il.smod) (i: Il.inst): unit =
   | Alloca a -> Il.smod_emit s (Printf.sprintf "/* alloca %%%d %s */" a.dest (Dtypes.type2str a.ty))
   in
   emit_newline s
+;;
 
 let emit_insts (s: Il.smod) (is: Il.insts): unit =
   let iterator = fun (i: Il.inst): unit ->
     emit_inst s i;
   in
+  Il.smod_emit s ".text\n";
   Array.iter iterator is
+;;
+
 let emit_constants (s: Il.smod): unit =
   let len: int = Array.length s.constants in
   let rec aux (i: int): unit =
@@ -214,4 +226,7 @@ let emit_constants (s: Il.smod): unit =
         (Printf.sprintf ".LK%d:\n\t.asciz\t\"%s\"\n" i str);
         aux (i + 1)
   in
+  if len > 0 then Il.smod_emit s ".section .rodata\n"
+  else ();
   aux 0
+;;
